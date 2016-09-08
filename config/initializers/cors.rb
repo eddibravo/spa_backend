3f8dca60cf63956067c61412522042f823488be7
bravo <eddi.bravo@ya.ru>
2016-09-08 21:59:12 +0300

    change origin domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.production?
-      origins 'static.spafrontend.s3-website-us-east-1.amazonaws.com'
+      origins 'd4xebji6ytazw.cloudfront.net'
     else
       origins 'localhost:4000', '127.0.0.1:4000'
     end
