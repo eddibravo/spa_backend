8c512511ea60ab9319b7a98fc6581bc332349258
bravo <eddi.bravo@ya.ru>
2016-09-21 01:38:00 +0300

    забыл убрать метод один, когда дебажил вручную...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,3 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
-
-  private
-  def authenticate_for entity_class
-    token = params[:token] || token_from_request_headers
-    return nil if token.nil?
-
-    begin
-      @entity = Knock::AuthToken.new(token: token).entity_for(entity_class)
-      define_current_entity_getter(entity_class)
-      @entity
-    rescue => ex
-      logger.error ex.message
-      nil
-    end
-  end
 end
